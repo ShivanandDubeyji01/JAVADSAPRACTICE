bea773d80badeab325cff97f6338ea618b1743b7
ShivanandDubeyji01 <89722709+ShivanandDubeyji01@users.noreply.github.com>
2023-02-28 19:17:36 +0530

    Update RunningSum.java

--- RunningSum.java
+++ RunningSum.java
@@ -2,15 +2,7 @@ import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class RunningSum {
-    static int[] sumarr(int a[]){
-        int arr[]= new int[a.length];
-        arr[0]=a[0];
-        for (int i = 1; i < arr.length; i++) {
-            arr[i]= arr[i-1]+a[i];
-        }
-        return arr;
-
-    }
+    
     static public int[] runningSum(int[] nums) {
         int []ans= new int[nums.length];
         ans[0]=nums[0];
